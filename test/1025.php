b392dcfe156d9c53e9d815917de015d3583cda99
floraLam <0603ljx@163.com>
2014-10-25 21:47:11 +0800

    v1

---
+++ test/1025.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+    <meta charset = "utf-8">
+    
+<body>
+    <?php
+         require '../utils/time.php';
+        /*
+         get serializable time while commonly used formats
+        */
+      /* function getSerTime(){
+           return date("Y-m-d h:i:sa");
+       }
+    */
+    echo getSerTime();
+    ?>
+</body>
+</html>
